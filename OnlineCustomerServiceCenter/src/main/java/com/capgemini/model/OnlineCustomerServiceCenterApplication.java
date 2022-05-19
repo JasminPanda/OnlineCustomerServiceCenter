a81f242c06a7d1e09d36f646ee129e3f198ce7c3
Kanchank11 <kanchansingh.in2@gmail.com>
2022-05-19 14:44:01 +0530

    make changes

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/OnlineCustomerServiceCenterApplication.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/OnlineCustomerServiceCenterApplication.java
@@ -2,8 +2,11 @@ package com.capgemini.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@SpringBootApplication(scanBasePackages = "com.capgemini.model")
+@EntityScan(basePackages = {"com.capgemini.model.entity"} )
+@EnableJpaRepositories(basePackages = {""})
 public class OnlineCustomerServiceCenterApplication {
 
 	public static void main(String[] args) {
--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Customer.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Customer.java
@@ -1,7 +1,10 @@
 package com.capgemini.model.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,13 +14,19 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-
+@Table(name="CustomerTable")
 public class Customer {
 	@Id
+	@Column
 	private int customerId;
+	@Column
 	private String firstName;
+	@Column
 	private String lastName;
+	@Column
 	private String email;
+	@Column
 	private String mobile;
+	@Column
 	private String city;
 }
