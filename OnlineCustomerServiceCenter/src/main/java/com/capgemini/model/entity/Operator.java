09f71259bf768c3e2f67e6508290c2698a09aa7f
JasminPanda <jasminpanda012@gmail.com>
2022-05-19 12:28:04 +0530

    with annotations

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Operator.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Operator.java
@@ -1,7 +1,17 @@
 package com.capgemini.model.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Operator {
+	@Id
 	private int operatorId;
 	private String firstName;
 	private String lastName;
@@ -9,56 +19,7 @@ public class Operator {
 	private String mobile;
 	private String city;
 	
-	public Operator() {
-		
-	}
 	
-	public int getOperatorId() {
-		return operatorId;
-	}
 	
-	public void setOperatorId(int operatorId) {
-		this.operatorId= operatorId;
-	}
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName= firstName;
-	}
-	
-	public String getLastName() {
-		return lastName;
-	}
-	
-	public void setLastName(String lastName) {
-		this.lastName=lastName;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email=email;
-	}
-	
-	public String getMobile() {
-		return mobile;
-	}
-	
-	public void setMobile(String mobile) {
-		this.mobile=mobile;
-	}
-	
-	public String getCity() {
-		return city;
-	}
-	
-	public void setCity(String city) {
-		this.city=city;
-	}
 	
 }
