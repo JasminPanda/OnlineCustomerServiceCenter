bd78bd330bb836cc9114b8836ad66da9595f4b45
chandana <srichandanakancharla630@gmail.com>
2022-05-19 11:16:30 +0530

    call

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Call.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Call.java
@@ -1,5 +1,49 @@
 package com.capgemini.model.entity;
 
-public class Call {
+import java.util.Date;
 
+public class Call {
+	int callid;
+	Date callDate;
+	double callDuration;
+	String phoneNumber;
+	public Call(int callid, Date callDate, double callDuration, String phoneNumber) {
+		super();
+		this.callid = callid;
+		this.callDate = callDate;
+		this.callDuration = callDuration;
+		this.phoneNumber = phoneNumber;
+	}
+	public int getCallid() {
+		return callid;
+	}
+	public void setCallid(int callid) {
+		this.callid = callid;
+	}
+	public Date getCallDate() {
+		return callDate;
+	}
+	public void setCallDate(Date callDate) {
+		this.callDate = callDate;
+	}
+	public double getCallDuration() {
+		return callDuration;
+	}
+	public void setCallDuration(double callDuration) {
+		this.callDuration = callDuration;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Call [callid=" + callid + ", callDate=" + callDate + ", callDuration=" + callDuration + ", phoneNumber="
+				+ phoneNumber + "]";
+	}
+	
 }
+
