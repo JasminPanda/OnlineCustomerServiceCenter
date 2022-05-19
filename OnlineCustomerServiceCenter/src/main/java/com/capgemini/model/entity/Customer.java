12f286496aa7ae0d36df20e022f61fd167de3444
Kanchank11 <kanchansingh.in2@gmail.com>
2022-05-19 18:02:11 +0530

    .

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Customer.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Customer.java
@@ -1,5 +1,35 @@
 package com.capgemini.model.entity;
 
-public class Customer {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.Data;
+@Data
+@Entity
+@Table
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+	private int customerId;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+	@Column
+	private String email;
+	@Column
+	private String mobile;
+	@Column
+	private String city;
+	
+	public Customer() {
+		super();
+	}
+	
+	
 }
