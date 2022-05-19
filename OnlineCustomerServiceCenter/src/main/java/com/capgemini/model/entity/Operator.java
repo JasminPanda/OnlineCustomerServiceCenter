faea1421732c8250eb61d061f91acd18610bdaf0
JasminPanda <jasminpanda012@gmail.com>
2022-05-19 13:00:50 +0530

    Operator

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Operator.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Operator.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+@Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
