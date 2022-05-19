0ee6ffbcd4a650c0befbc65adbc40bef73ca1390
Kanchank11 <kanchansingh.in2@gmail.com>
2022-05-19 13:22:18 +0530

    message

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Customer.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Customer.java
@@ -2,14 +2,16 @@ package com.capgemini.model.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+@Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+
 public class Customer {
 	@Id
 	private int customerId;
