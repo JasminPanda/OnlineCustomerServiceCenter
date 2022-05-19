2c7cc3059c8375801b65768fb7f4aef8eccee649
Kanchank11 <kanchansingh.in2@gmail.com>
2022-05-19 17:01:41 +0530

    .

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/OnlineCustomerServiceCenterApplication.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/OnlineCustomerServiceCenterApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(scanBasePackages = "com.capgemini.model")
 @EntityScan(basePackages = {"com.capgemini.model.entity"} )
-@EnableJpaRepositories(basePackages = {""})
+@EnableJpaRepositories(basePackages = {"com.capgemini.model.repository"})
 public class OnlineCustomerServiceCenterApplication {
 
 	public static void main(String[] args) {
