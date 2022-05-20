542bb13af3de7017bb9297b8e92784a063fc3638
spmaddel <spmaddel@INLEN8520051907.corp.capgemini.com>
2022-05-20 12:58:21 +0530

    Project

---
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Department.java
@@ -0,0 +1,17 @@
+package com.capgemini.model.entity;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Department {
+
+}
