837b720a2f5e537f3f5176d0eb2729ddb1546b72
spmaddel <spmaddel@INLEN8520051907.corp.capgemini.com>
2022-05-23 11:08:13 +0530

    Department

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Department.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/Department.java
@@ -1,5 +1,32 @@
 package com.capgemini.model.entity;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+@Data
+@Entity
+@Table
 
 public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column
+     private int deptId;
+	@Column
+     private String customerName;
+	@Column
+     private String phoneNumber;
+	@Column
+     private boolean isActive;
+	
+	public Department() {
+		super();
+	}
 
 }
