4322b85a2486ab257a00c0c5250d5025b36af43f
chandana <srichandanakancharla630@gmail.com>
2022-05-23 17:31:48 +0530

    Customer

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/controller/CustomerController.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/controller/CustomerController.java
@@ -1,8 +1,20 @@
 package com.capgemini.model.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capgemini.model.dto.CustomerDto;
+import com.capgemini.model.dto.Issue;
+import com.capgemini.model.dto.IssueDto;
+import com.capgemini.model.dto.LoginDto;
+import com.capgemini.model.dto.Response;
 import com.capgemini.model.service.CustomerService;
 
 @RestController("customers")
@@ -10,5 +22,30 @@ public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
-
+	
+	@GetMapping(value="/{issueId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response<IssueDto> viewIssueById(@PathVariable("issueId") Integer issueId){
+		return customerService.viewIssueById(issueId);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response<Issue> reOpenIssue(@RequestBody Issue issue){
+		return customerService.reopenIssue(issue);
+	}
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response<List<Issue>> viewAllIssues(@RequestBody Issue issue){
+		return customerService.viewAllIssues();
+	}
+	@GetMapping(value="/{password}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response<LoginDto>changePassword(@PathVariable("password") String password){
+		return customerService.changePassword(password);
+	}
+	@GetMapping(value="/{password}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response<LoginDto>forgetPassword(@PathVariable("password") String password){
+		return customerService.forgetPassword(password);
+	}
+	@GetMapping(value="/{email}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Response<CustomerDto>emailPassword(@PathVariable("email") int email){
+		return customerService.emailPassword(email);
+	}
 }
--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/service/CustomerService.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/service/CustomerService.java
@@ -2,20 +2,23 @@ package com.capgemini.model.service;
 
 import java.util.List;
 import com.capgemini.model.dto.IssueDto;
+import com.capgemini.model.dto.CustomerDto;
 import com.capgemini.model.dto.Issue;
 import com.capgemini.model.dto.LoginDto;
+import com.capgemini.model.dto.Response;
 import com.capgemini.model.entity.Customer;
 
 public interface CustomerService {
 
 	public String login(LoginDto login);
 	public String registerCustomer(Customer customer);
-	public IssueDto viewIssueById(IssueDto issue);
-	public Issue viewIssueById(Issue issue);
-	public Issue reopenIssue(int issue);
-	public List<Issue> viewAllIssues();
-	public String changePassword(LoginDto login);
+	public Response<IssueDto>viewIssueById(int issueId);
+	public Response<Issue> reopenIssue(Issue issue);
+	public Response<List<Issue>> viewAllIssues();
+	public Response<LoginDto> changePassword(String password);
 	public String forgetPassword(int password);
-	public Customer emailPassword(int email);
-
+	public Response<CustomerDto> emailPassword(int email);
+	public Response<LoginDto> forgetPassword(String password);
+	
+	
 }
