0bd7351075be59a860e3d011b9235f767c3048f9
JasminPanda <jasminpanda012@gmail.com>
2022-05-23 16:21:53 +0530

    Solution class

--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/dto/SolutionDto.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/dto/SolutionDto.java
@@ -1,5 +1,16 @@
 package com.capgemini.model.dto;
 
-public class SolutionDto {
+import java.util.Date;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class SolutionDto {
+		private int solutionId;
+		private String solutionDescription;
+		private Date solutionDate;
 }
--- OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/SolutionEntity.java
+++ OnlineCustomerServiceCenter/src/main/java/com/capgemini/model/entity/SolutionEntity.java
@@ -1,5 +1,29 @@
 package com.capgemini.model.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@NoArgsConstructor
+@ToString
+@Table(name = "solution")
 public class SolutionEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "solutionId")
+	private int solutionId;
+	private String solutionDescription;
+	private Date solutionDate;
 
 }
